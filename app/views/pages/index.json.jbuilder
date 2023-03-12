d9ddfad9f6267b5d85ef7ae21ef76fbdbb19dd39
jmowrey27 <jmowrey27@gmail.com>
2023-03-11 18:40:53 -0700

    adding basic customizable pages

---
+++ app/views/pages/index.json.jbuilder
@@ -0,0 +1,1 @@
+json.array! @pages, partial: "/page", as: :page
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  
+  devise_for :admins 
+    
   root   'static_pages#home'
   get    'static_pages/home'
   get    '/help',              to: "static_pages#help"
   get    '/about',             to:  "static_pages#about"
   get    '/contact',           to: "static_pages#contact"
+  get    '/pages',             to:  "pages#index"
+  resources :pages
+
 end
