7721a8f2a0238668298fabb071a528d2ab8040b8
jmowrey27 <jmowrey27@gmail.com>
2023-06-23 12:22:35 -0600

    adding carosel and video

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,7 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'videos')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
@@ -17,3 +18,5 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap
 Rails.application.config.assets.precompile += %w( ckeditor/* )
 Rails.application.config.assets.precompile += %w(custom.scss)
 Rails.application.config.assets.precompile += ['solidus_starter_frontend_manifest.js']
+Rails.application.config.assets.precompile += %w( *.mp4 *.webm *.ogv )
+Rails.application.config.assets.precompile += %w( *.png *.jpg *.jpeg *.gif *.svg *.ico *.eot *.ttf *.woff *.woff2 *.otf )
